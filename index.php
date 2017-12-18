1deebc6e5adc401bbaf84b2fff243848e7b893d8
roygala <lucas@hi-1.dem>
2017-12-18 18:41:59 +0100

    xdxd

--- index.php
+++ index.php
@@ -89,9 +89,11 @@
 		<div class="centered" id="pageInfo">
 			<span class="mayhem">Mayhem</span> <span class="mayhemSmall">//your #1 HvH tranny</span><br />
 			<div class="links">
-				<a href="http://steamcommunity.com/id/flickz_de" target="_blank">//Steam</a>
-				<a href="http://steamcommunity.com/id/mayhemakafag" target="_blank">//Steam Wingman</a>
-				<a href="https://paste.plsrape.me/user.php?user=mayhem" target="_blank">//My configs</a> <span class="zeus">a</span>
+				<center>
+				<a href="http://steamcommunity.com/id/flickz_de" target="_blank">Steam</a> • 
+				<a href="http://steamcommunity.com/id/mayhemakafag" target="_blank">Steam Wingman</a> • 
+				<a href="https://paste.plsrape.me/user.php?user=mayhem" target="_blank">My configs</a> <span class="zeus">a</span>
+				</center>
 			</div>
 		</div>
 	</body>
