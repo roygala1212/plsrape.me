76e841a7d491aad068b85451536c2a4ab33db207
roygala <lucas@hi-1.dem>
2017-12-18 19:37:38 +0100

    xd

--- index.php
+++ index.php
@@ -4,74 +4,9 @@
 		<title>Mayhem</title>
 		<link rel="stylesheet" type="text/css" href="./src/css/style.css" />
 		<link rel="icon" type="image/jpeg" href="./src/media/icon.jpg" />
-		<script src="./src/js/jquery-3.2.1.min.js"></script>
-		<script src="./src/js/jquery.color-2.1.2.min.js"></script>
-		<script>
-			var pageTitle = "-----Mayhem-----";
-			function animateTitle() {
-				pageTitle = pageTitle.substring(1, pageTitle.length) + pageTitle.substring(0, 1);
-				document.title = pageTitle;
-				setTimeout("animateTitle()", 300);
-			}
-			$(document).ready(function() {
-				animateTitle();
-				
-				$(".mayhem").animate({
-					opacity: 1
-				}, 500, function() {
-					$(".mayhemSmall").animate({
-						opacity: 1
-					}, 500, function() {
-						$(".links").animate({
-							opacity: 1
-						}, 500);
-					});
-				});
-				
-				function loop() {
-					$(".mayhem").animate({
-						color: "#c64800"
-					}, 1500, function() {
-						$(".mayhem").animate({
-							color: "#a8c600"
-						}, 1500, function() {
-							$(".mayhem").animate({
-								color: "#00c67d"
-							}, 1500, function() {
-								$(".mayhem").animate({
-									color: "#009ec6"
-								}, 1500, function() {
-									$(".mayhem").animate({
-										color: "#7000c6"
-									}, 1500, function() {
-										$(".mayhem").animate({
-											color: "#c6007d"
-										}, 1500, function() {
-											loop();
-										});
-									});
-								});
-							});
-						});
-					});
-				}
-				loop();
-				
-				$("a").hover(function() {
-					$(this).stop();
-					$(this).animate({
-						color: "#b2ff00"
-					}, 150);
-				});
-				
-				$("a").mouseout(function() {
-					$(this).stop();
-					$(this).animate({
-						color: "#ff6100"
-					}, 250);
-				});
-			});
-		</script>
+		<script src="./src/js/jquery-3.2.1.min.js" />
+		<script src="./src/js/jquery.color-2.1.2.min.js" />
+		<script src="./src/js/mayhem.js" />
 		<!--[if lt IE 9]
 		<script>
 			document.createElement('video');
---
+++ src/js/mayhem.js
@@ -0,0 +1,64 @@
+var pageTitle = "-----Mayhem-----";
+function animateTitle() {
+    pageTitle = pageTitle.substring(1, pageTitle.length) + pageTitle.substring(0, 1);
+    document.title = pageTitle;
+    setTimeout("animateTitle()", 300);
+}
+$(document).ready(function() {
+    animateTitle();
+    
+    $(".mayhem").animate({
+        opacity: 1
+    }, 500, function() {
+        $(".mayhemSmall").animate({
+            opacity: 1
+        }, 500, function() {
+            $(".links").animate({
+                opacity: 1
+            }, 500);
+        });
+    });
+    
+    function loop() {
+        $(".mayhem").animate({
+            color: "#c64800"
+        }, 1500, function() {
+            $(".mayhem").animate({
+                color: "#a8c600"
+            }, 1500, function() {
+                $(".mayhem").animate({
+                    color: "#00c67d"
+                }, 1500, function() {
+                    $(".mayhem").animate({
+                        color: "#009ec6"
+                    }, 1500, function() {
+                        $(".mayhem").animate({
+                            color: "#7000c6"
+                        }, 1500, function() {
+                            $(".mayhem").animate({
+                                color: "#c6007d"
+                            }, 1500, function() {
+                                loop();
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    }
+    loop();
+    
+    $("a").hover(function() {
+        $(this).stop();
+        $(this).animate({
+            color: "#b2ff00"
+        }, 300);
+    });
+    
+    $("a").mouseout(function() {
+        $(this).stop();
+        $(this).animate({
+            color: "#ff6100"
+        }, 300);
+    });
+});
